1d435dce770cf9ba567a93e2186ff185157ae485
Eiko Ujifusa <eikorouji@gmail.com>
2022-04-08 13:25:10 -0700

    added queries to create, delete, and call a single candidate from db

--- server.js
+++ server.js
@@ -8,6 +8,52 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to the database //
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username //
+        user: 'root',
+        // MySQL pword //
+        password: 'password',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+
+// get a single candidate //
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+// Delete a candidate //
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+// create a candidate //
+const sql =  `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+  values (?,?,?,?)`;
+
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+
+
+
 // Default response for any other request (Not Found) //
 app.use((req, res) => {
     res.status(404).end();
